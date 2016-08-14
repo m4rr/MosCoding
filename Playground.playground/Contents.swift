0e3f605b4dcf8b7bd35bf36844b99f62adc4b3a7
Marat Saytakov <remarr@gmail.com>
2016-08-14 17:57:32 +0300

    filter map

--- Playground.playground/Contents.swift
+++ Playground.playground/Contents.swift
@@ -45,3 +45,153 @@ image.size
 
 
 
+var name: String? = "Yes"
+
+name = nil
+
+//print(name?.characters.count)
+
+let aNum1 = 3
+let aNum2: Int? = nil
+let aNum3 = Optional(3)
+
+//Int.max
+
+//print(aNum1.advanced(by: 5))
+//print(aNum2?.advanced(by: 5))
+//print(aNum3?.advanced(by: 5))
+
+name == nil
+name != ""
+
+aNum2 == nil
+//aNum1 == nil
+
+name = "Marat"
+
+if name == nil {
+  //print("not a name")
+} else {
+  //print(name)
+}
+
+if let innerName = name {
+  //  print(1, name)
+  //  print(2, innerName)
+} else {
+  //  print(3, "not a name")
+}
+
+
+var array = [1, 2, 3, 4, 5]
+
+for number in array {
+  //  print(number)
+}
+
+//print(array.first)
+
+array = []
+
+//print(array.first)
+
+
+
+typealias CityType = (cityName: String, temp: Int, pressure: Int)
+
+var cities: [CityType] = [
+
+  ("Moscow",    20, 751),
+  ("London",    30, 752),
+  ("Berlin",     0, 752),
+  ("Amsterdam", 15, 750),
+
+]
+
+cities.first
+cities.last
+
+cities[0]
+cities[1...3]
+
+let capitals = [
+  "Moscow": "Russia",
+  "London": "UK",
+  "Berlin": "Germany",
+  "Amsterdam": "The Netherlands"
+]
+
+
+for city in cities {
+  print(city.cityName, " \t ",
+        capitals[city.cityName] ?? "not found")
+}
+
+print()
+
+for (key, value) in capitals {
+  print(value)
+}
+
+typealias СityType = (cityName: String, temp: Int, pressure: Int)
+
+var сities: [CityType] = [
+
+  ("Moscow",    20, 751),
+  ("London",    30, 752),
+  ("Berlin",     0, 752),
+  ("Amsterdam", 15, 750),
+
+]
+
+
+let filtered =
+
+  cities.filter { (t: CityType) -> Bool in
+
+    return t.cityName.contains("e")
+
+    return t.pressure >= 752 && t.temp >= 20
+}
+
+func nameCriteria(t: CityType) -> Bool {
+  return t.cityName.contains("e")
+}
+
+func weatherCriteria(t: CityType) -> Bool {
+  return t.pressure >= 752 && t.temp >= 20
+}
+
+cities
+  .filter(nameCriteria)
+  .filter(weatherCriteria)
+
+cities.map { (t: CityType) -> String in
+  return t.cityName
+}
+
+func nameExtractor(t: CityType) -> String {
+  return t.cityName
+}
+
+cities.map(nameExtractor)
+
+
+//let something = criteria()
+
+true && true
+true || false
+!true
+true != true
+true == true
+!(true != true) && false
+
+(1 + 1) * 0
+
+print(filtered)
+
+
+
+
+
+
