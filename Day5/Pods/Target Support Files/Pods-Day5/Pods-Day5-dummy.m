636965938eed21f1dc52a093e8113a8dc2798995
Marat Saytakov <remarr@gmail.com>
2016-11-06 12:03:12 +0300

    Fold

--- Day5/Pods/Target Support Files/Pods-Day5/Pods-Day5-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Day5 : NSObject
-@end
-@implementation PodsDummy_Pods_Day5
-@end
--- Day5/Pods/Target Support Files/Pods-Day5/Pods-Day5-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_Day5VersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_Day5VersionString[];
-
