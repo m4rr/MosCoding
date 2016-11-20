da0a3f5881bc4be5350c22b717a386e1845c729f
Marat Saytakov <remarr@gmail.com>
2016-11-20 19:22:27 +0300

    gesture recognizer

--- Flashcards/Flashcards/AnimatedViewController.swift
+++ Flashcards/Flashcards/AnimatedViewController.swift
@@ -11,7 +11,8 @@ import UIKit
 class AnimatedViewController: UIViewController {
 
   @IBOutlet weak var definitionLabel: UILabel!
-  // @IBOutlet weak var topConstraint
+
+  @IBOutlet weak var topConstraint: NSLayoutConstraint!
 
   var flashcard: Flashcard?
 
@@ -19,18 +20,44 @@ class AnimatedViewController: UIViewController {
     super.viewDidLoad()
 
     definitionLabel.text = nil
+
   }
 
   override func viewDidAppear(_ animated: Bool) {
     super.viewDidAppear(animated)
 
     UIView.transition(with: definitionLabel,
-                      duration: 0.3, 
+                      duration: 0.3,
                       options: [.curveEaseOut, .transitionFlipFromBottom],
                       animations: {
                         self.definitionLabel.text = self.flashcard?.definition
                       },
                       completion: nil)
+
+//    topConstraint.constant = 400
+
+//    UIView.animate(withDuration: 2, delay: 1, animations: {
+//      self.view.layoutIfNeeded()
+//    })
+
+  }
+  
+  @IBAction func handlePan(_ sender: UIPanGestureRecognizer) {
+//    print(sender.velocity(in: self.view))
+
+    let point = sender.location(in: view)
+
+
+    let navbarHeight = navigationController?.navigationBar.frame.height ?? 0
+
+    topConstraint.constant = point.y - navbarHeight
+
+    UIView.animate(withDuration: 0.1, animations: {
+      self.view.layoutIfNeeded()
+    })
+
+
+
   }
 
 }
